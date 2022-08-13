4b4e7b3753bbbd7b7bdbc75033b5917466b5a5c0
Subhadeep Dan <dansubhadeep@gmail.com>
2022-08-13 16:55:19 +0530

    Stack Implementation using LinkedList

--- src/StackUsingLinkedList.java
+++ src/StackUsingLinkedList.java
@@ -11,21 +11,61 @@ public class StackUsingLinkedList {
         }
     }
 
+    StackUsingLinkedList(){
+        head = null;
+    }
+
     static void push(int newData){
         Node newNode = new Node(newData);
         newNode.next = head;
         head = newNode;
     }
 
-    static void pop(){
+    static int pop(){
         if(head==null){
             System.out.print("Top is empty");
-            return;
+            return -1;
         }
+        int element = head.data;
         head = head.next;
+        return element;
+    }
+
+    static int peek(){
+        if(head==null){
+            System.out.print("Top is empty");
+            return -1;
+        }
+        return head.data;
+    }
+
+    static boolean isEmpty(){
+        if(head==null){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public static void main(String[] args){
+        StackUsingLinkedList stack = new StackUsingLinkedList();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
 
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
+
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.isEmpty());
     }
 }
